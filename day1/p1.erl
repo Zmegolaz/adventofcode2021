b14baa17872e1edd2d489973f637396187ae2200
Oscar Ekeroth <oekeroth@baffinbaynetworks.com>
2021-12-09 12:00:27 +0100

    day1

---
+++ day1/p1.erl
@@ -0,0 +1,32 @@
+-module(p1).
+-export([start/0]).
+
+start() ->
+	{ok, InputFile} = file:open('input', [read]),
+	FirstLine = file:read_line(InputFile),
+	SecondLine = file:read_line(InputFile),
+	countIncreases(InputFile, FirstLine, SecondLine, 0),
+	file:close(InputFile),
+	ok.
+
+countIncreases(_, _, eof, Increases) ->
+	io:format("~s~n", [integer_to_list(Increases)]),
+	ok;
+
+% Skip empty lines.
+countIncreases(InputFile, {ok, OldLine}, {ok, [10]}, Increases) ->
+	countIncreases(InputFile, {ok, OldLine}, file:read_line(InputFile), Increases),
+	ok;
+
+countIncreases(InputFile, {ok, OldLine}, {ok, NewLine}, Increases) ->
+	{OldLineInt, _} = string:to_integer(OldLine),
+	{NewLineInt, _} = string:to_integer(NewLine),
+	case OldLineInt < NewLineInt of
+		true ->
+			countIncreases(InputFile, {ok, NewLine}, file:read_line(InputFile), Increases+1);
+		false ->
+			countIncreases(InputFile, {ok, NewLine}, file:read_line(InputFile), Increases)
+	end,
+	ok.
+
+
---
+++ day1/p2.erl
@@ -0,0 +1,34 @@
+-module(p2).
+-export([start/0]).
+
+start() ->
+	{ok, InputFile} = file:open('input', [read]),
+	{ok, FirstLine} = file:read_line(InputFile),
+	{FirstLineInt, _} = string:to_integer(FirstLine),
+	{ok, SecondLine} = file:read_line(InputFile),
+	{SecondLineInt, _} = string:to_integer(SecondLine),
+	{ok, ThirdLine} = file:read_line(InputFile),
+	{ThirdLineInt, _} = string:to_integer(ThirdLine),
+	FourthLine = file:read_line(InputFile),
+	countIncreases(InputFile, FirstLineInt, SecondLineInt, ThirdLineInt, FourthLine, 0),
+	file:close(InputFile),
+	ok.
+
+countIncreases(_, _, _, _, eof, Increases) ->
+	io:format("~s~n", [integer_to_list(Increases)]),
+	ok;
+
+% Skip empty lines.
+countIncreases(InputFile, FirstLine, SecondLine, ThirdLine, {ok, [10]}, Increases) ->
+	countIncreases(InputFile, FirstLine, SecondLine, ThirdLine, file:read_line(InputFile), Increases),
+	ok;
+
+countIncreases(InputFile, FirstLineInt, SecondLineInt, ThirdLineInt, {ok, FourthLine}, Increases) ->
+	{FourthLineInt, _} = string:to_integer(FourthLine),
+	case FirstLineInt + SecondLineInt + ThirdLineInt < SecondLineInt + ThirdLineInt + FourthLineInt of
+		true -> NewIncreases = Increases + 1;
+		false -> NewIncreases = Increases
+	end,
+	countIncreases(InputFile, SecondLineInt, ThirdLineInt, FourthLineInt, file:read_line(InputFile), NewIncreases),
+	ok.
+
